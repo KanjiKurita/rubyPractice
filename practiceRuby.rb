3d665cdb62260638edfb245837910cd091db10e1
KanjiKurita <coderkkurita@gmail.com>
2020-03-28 01:19:44 +0900

    関数つかったった

--- practiceRuby.rb
+++ practiceRuby.rb
@@ -15,11 +15,37 @@ print text
 print File.read(ARGV[1])
 =end
 
+#各行を表示
+=begin
 filename=ARGV[0]
 file=File.open(filename)
 file.each_line do |line|
      print line
 end
 file.close
+=end
+
+=begin
+#grep　パターンに一致した行だけ表示
+pattern=Regexp.new(ARGV[0])
+filename=ARGV[1]
+file =File.open(filename)
+file.each_line do |line|
+    if pattern=~line
+        print line
+    end
+end
+file.close
+=end
+
+def hello
+    puts "Hello Ruby(気さくな挨拶)"
+end
+
+hello()
+
+
+
+
 
 
