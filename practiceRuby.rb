d6cdeef60ce297584975f39a73a12cc2cfaf0490
KanjiKurita <coderkkurita@gmail.com>
2020-03-28 01:03:51 +0900

    各行表示

--- practiceRuby.rb
+++ practiceRuby.rb
@@ -1,5 +1,9 @@
 #hello git hub, in ruby.
 
+=begin    
+rescue => exception
+    
+end
 puts "hello, github with ruby, using vscode"
 puts "いずれのおおんときにか、やんごとなき" #日本語テスト
 puts "branchしてcheckoutしたぞ" #git動作確認
@@ -9,5 +13,13 @@ text=File.read(filename)
 print text
 
 print File.read(ARGV[1])
+=end
+
+filename=ARGV[0]
+file=File.open(filename)
+file.each_line do |line|
+     print line
+end
+file.close
 
 
