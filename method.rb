c399b4dcf37f32ab586a88ed1c8f0d3c6498c22e
KanjiKurita <coderkkurita@gmail.com>
2020-03-30 01:20:16 +0900

    method

---
+++ method.rb
@@ -0,0 +1,13 @@
+def shoukan(name="アーチャー")
+    puts "サーヴァント、#{name}。召喚に応じ、はせ参じた。"
+end
+shoukan()
+shoukan("セイバー")
+
+hash={saber:"アルトリア",archer:"エミヤ",lancer:"クーフーリン"}
+
+def staynight(eirei)
+    eirei
+end
+
+puts staynight(hash)
